aaf23dbad03e8bd346e09be8a4c8d7f1bddf955d
OmegaWizard-107 <holland.118@wright.edu>
2025-09-12 20:40:06 -0400

    Added doccumentation

--- Lab2/src/Driver.java
+++ Lab2/src/Driver.java
@@ -1,21 +1,27 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Driver {
     public static void main(String[] args) {
 
         ArrayList<Golfer> golfers = new ArrayList<Golfer>();
         
+        // Creates 3 new golfers 
         Golfer g1 = new Golfer("Jay", "Smith", -13 , 17);
         Golfer g2 = new Golfer("DeShaun", "Smith", -11 , 16);
         Golfer g3 = new Golfer("DeShaun", "Tayor", -11 , 2);
-
-        golfers.add(g1);
+        
+        //adds golfers to the array in an unsorted order
         golfers.add(g2);
+        golfers.add(g1);
         golfers.add(g3);
 
+        //prints the unsorted array
         System.out.println(golfers.toString());
 
+        //sorts then prints the array
         Collections.sort(golfers);
         System.out.println(golfers.toString());
     }
 }
+    
--- Lab2/src/Golfer.java
+++ Lab2/src/Golfer.java
@@ -1,10 +1,22 @@
-public class Golfer implements Comparable {
+/**
+ * Golfer class that keps track of the golfers First and Last name, their score, and how many holes they completed
+ * Has the ability to sort golfers by first their score, then their holes completed, then by name.
+ */
+
+public class Golfer implements Comparable<Golfer> {
     private String firstName;
     private String lastName;
     private int score; // golfer’s score so far this round, more negative is better
     private int holesCompleted; // number of holes golfer has completed so far this round
 
-    // constructor that takes in four parameters to initialize the fields of this object
+
+    /**
+     * Constructor that takes in four parameters to initialize the fields of this object
+     * @param firstName first name of the golfer
+     * @param lastName last name of the golfer
+     * @param score golfer’s score so far this round, more negative is better
+     * @param holesCompleted number of holes golfer has completed so far this round
+     */
     public Golfer(String newFirstName, String newLastName, int newScore, int newHolesCompleted) {
         firstName = newFirstName;
         lastName = newLastName;
@@ -12,23 +24,32 @@ public class Golfer implements Comparable {
         holesCompleted = newHolesCompleted;
     }
 
-    // returns a String of the form:
-    // `lastName`, `firstName`: `score` with `holesCompleted` holes completed
+    /** 
+     * returns a String of the form:
+     * `lastName`, `firstName`: `score` with `holesCompleted` holes completed
+    */ 
     public String toString() {
         return lastName + ", " + firstName + ": " + score + " with " + holesCompleted + " holes completed";
     }
 
+    /**
+     * Sorts golfers in the order of:
+     * More negative score
+     * More holes completed
+     * Last name alphabetically
+     * First name alphabetically
+     */
+
     @Override
     public int compareTo(Golfer other) {
         if (this.score != other.score) {
-            return Integer.compare(this.score, other.score); // lower score first
+            return Integer.compare(this.score, other.score); // sorts by score (more negative comes first)
         }
-        if (this.holesCompleted != other.holesCompleted) {
-            return Integer.compare(other.holesCompleted, this.holesCompleted); // higher holes first
+        if (this.holesCompleted != other.holesCompleted) { 
+            return Integer.compare(other.holesCompleted, this.holesCompleted); // sorts by the number of holes completed (higher comes first)
         }
-        int lastCmp = this.lastName.compareToIgnoreCase(other.lastName);
-        if (lastCmp != 0) {
-            return lastCmp;
+        if (this.lastName.compareToIgnoreCase(other.lastName) != 0) {
+            return this.lastName.compareToIgnoreCase(other.lastName); // sorts lexicographically by last name (ignoring case)
         }
         return this.firstName.compareToIgnoreCase(other.firstName);
     }
